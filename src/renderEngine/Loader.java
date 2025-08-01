9097c6e24d1fafd173d27a01374c941747a25f40
sudatra <mail2sudatra@gmail.com>
2025-08-01 19:54:47 +0530

    added index buffers

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -7,6 +7,7 @@ import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 
 import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,12 +15,14 @@ public class Loader {
 	private List<Integer> vaos = new ArrayList<Integer>();
 	private List<Integer> vbos = new ArrayList<Integer>();
 	
-	public RawModel loadToVAO(float[] positions) {
+	public RawModel loadToVAO(float[] positions, int[] indices) {
 		int vaoID = createVAO();
+		
+		bindIndicesBuffer(indices);
 		storeDataInAttributeList(0, positions);
 		unbindVAO();
 		
-		return new RawModel(vaoID, positions.length / 3);
+		return new RawModel(vaoID, indices.length);
 	}
 	
 	public void cleanUp() {
@@ -53,6 +56,25 @@ public class Loader {
 	
 	private void unbindVAO() {
 		GL30.glBindVertexArray(0);
+	}
+	
+	private void bindIndicesBuffer(int[] indices) {
+		int vboID = GL15.glGenBuffers();;
+		vbos.add(vboID);
+		
+		GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, vboID);
+		IntBuffer buffer = storeDataInIntBuffer(indices);
+		GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+		
+	}
+	
+	private IntBuffer storeDataInIntBuffer(int[] data) {
+		IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
+		
+		buffer.put(data);
+		buffer.flip();
+		
+		return buffer;
 	}
 	
 	private FloatBuffer storeDataInFloatBuffer(float[] data) {
